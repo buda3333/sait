1816459ca5d06e30378679919c791c037ec79592
buda3333 <sushkeevbuda03@gmail.com>
2023-07-10 18:02:51 +0800

    разнесли все по отдельным файлом

---
+++ app/public/handlers/login.php
@@ -0,0 +1,51 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $conn = new PDO(dsn: 'pgsql:host=db;dbname=dbname', username: 'dbuser', password: 'dbpwd');
+    $errors = isValidLogin($_POST, $conn);
+
+    if (empty($errors)) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $stmt = $conn->prepare("SELECT * FROM users WHERE email = :email");
+        $stmt->execute(['email' => $email]);
+        $userData = $stmt->fetch();
+        if (!empty($userData) && (password_verify($password, $userData['password'])))
+        {
+            session_start();
+            $_SESSION['user'] = ['email' => $userData['email']];
+            header('Location: ./main');
+        } else {
+            $errorsLogin['errors'] = '* Неверный пароль';
+        }
+
+    }
+}
+    require_once './htmlcod/login.html';
+function isValidLogin(array $data,PDO $conn) :array {
+    $errors = [];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if (!isset($email)) {
+        $errors['email'] = 'email is required';
+    }
+    if (empty($email)) {
+        $errors['email'] = 'email не доджно быть пустой';
+    }
+
+    if (!isset($password)) {
+        $errors['password'] = 'password is required';
+    }
+    if (empty($password)) {
+        $errors['password'] = 'password не доджно быть пустой';
+    }
+
+    $stmt = $conn->prepare("SELECT * FROM users WHERE email = :email");
+    $stmt->execute(['email' => $email]);
+    $userData = $stmt->fetch();
+    if (empty($userData)) {
+        $errors['email'] = 'Пользователь не зарегестрирован c таким email';
+    }
+    return $errors;
+}
+?>
