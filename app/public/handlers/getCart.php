3bd3f276d878d7018f16b75fc32ce942e64a83e9
buda3333 <sushkeevbuda03@gmail.com>
2023-07-16 20:48:00 +0800

    get_Cart4

--- app/public/handlers/getCart.php
+++ app/public/handlers/getCart.php
@@ -6,21 +6,27 @@ if (!isset($_SESSION['user_id'])) {
 $conn = new PDO(dsn: 'pgsql:host=db;dbname=dbname', username: 'dbuser', password: 'dbpwd');
 $session_id = $_SESSION['user_id'];
 
-$stmt = $conn->prepare("SELECT * FROM cart WHERE user_id = :user_id");
+/*$stmt = $conn->prepare("SELECT * FROM cart WHERE user_id = :user_id");
 $stmt->bindParam(':user_id', $session_id);
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 foreach ($result as $row) {
     //echo "ID: " . $row['id'] . "<br>";
     //echo "User ID: " . $row['user_id'] . "<br>";
-    echo "Товар: " . getProductName($row['product_id']) . "<br>";
-    echo "Количество: " . $row['quantity'] . "<br>";
-}
-$totalSum = 0;
-$productSum = $row['quantity'] * getProductPrice($row['product_id']);
-$totalSum += $productSum;
-echo "Общая сумма: " . $totalSum;
-//require_once './htmlcod/getCart.phtml';
+    //echo "Товар: " . getProductName($row['product_id']) . "<br>";
+    //echo "Количество: " . $row['quantity'] . "<br>";
+}*/
+$query = "SELECT tovary.name, cart.quantity, tovary.price * cart.quantity AS total_price
+FROM cart INNER JOIN tovary ON cart.product_id = tovary.id WHERE user_id = :user_id";
+$stmt = $conn->prepare($query);
+$stmt->bindValue(':user_id', $_SESSION['user_id']);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//$totalSum = 0;
+//$productSum = $res['quantity'] * getProductPrice($res['product_id']);
+//$totalSum += $productSum;
+//echo "Общая сумма: " . $totalSum;
+require_once './htmlcod/getCart.phtml';
 function getProductPrice($product_id) {
     $conn = new PDO(dsn: 'pgsql:host=db;dbname=dbname', username: 'dbuser', password: 'dbpwd');
     $stmt = $conn->prepare("SELECT price FROM tovary WHERE id = :product_id");
@@ -40,15 +46,4 @@ $conn = new PDO(dsn: 'pgsql:host=db;dbname=dbname', username: 'dbuser', password
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     return $result['name'];
 }
-
-// Отформатировать данные в HTML-код
-//$html = '<h1>Корзина</h1>';
-//$html .= '<ul>';
-//foreach ($cartItems as $item) {
-  //  $html .= '<li>' . $item['name'] . ': ' . $item['quantity'] . '</li>';
-//}
-//$html .= '</ul>';
-
-// Вернуть HTML-код
-//echo $html;
 ?>
--- app/public/handlers/showCart().js
+++
@@ -1,8 +0,0 @@
-function showCart() {
-    fetch('/getCart.php') // Путь к файлу, который будет возвращать данные о корзине
-        .then(response => response.text())
-        .then(data => {
-            // Отобразить данные о корзине в сплывающем окне
-            var popup = window.open('', 'cartPopup', 'width=400,height=300');
-            popup.document.write(data);
-        });
